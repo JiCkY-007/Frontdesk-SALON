8b032a7c73998b29cfb45f35eca1c89141803407
Jicky007 <148865124+Jicky007@users.noreply.github.com>
2025-05-11 22:29:45 +0530

    Intial submit-1

--- AI-livekit/constants.py
+++ AI-livekit/constants.py
@@ -1,2 +1,4 @@
+# Change these paths to your own
+git commit -m "Initial commit"
 path_to_yaml = "C://Users//jicky//OneDrive//Desktop//AI_FrontDesk//AI-livekit//promt.yaml"
 request_url_to_notify = "http://localhost:5000/api/requests"
